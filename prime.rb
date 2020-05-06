254d08c51c865aa7e479c3e47cd062afb2fcb611
Kevin Boston <kbos22@gmail.com>
2020-05-06 12:11:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(num)
-  (2..(num - 1)).each do |n|
-     if (num % n == 0)
+  (2..(num - 1)).to_a.all? do |n|
+     if (num % n != 0)
        false
      else
        true
